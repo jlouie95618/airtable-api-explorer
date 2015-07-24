8556941f26ae077fa6270f72cf024b9a5a81f112
John Louie <jlouie95618@gmail.com>
2015-07-24 16:53:45 -0700

    made a decent number of style and html changes; we now have a decent framework for our work

--- js/test.js
+++ js/test.js
@@ -5,8 +5,21 @@ console.log(_);
 
 var requestify = require('./index.js');
 
-$(document).on('change', '.requestTypes', function(eventData) {
-    var selectedValue = $('.requestTypes').val();
+$(document).on('change', '.api-version', function(eventData) {
+    var selectedValue = $('.api-version').val();
+    if (selectedValue === 'curl') {
+        console.log('curl happened!');
+        $('.api-version-curl').show();
+        $('.api-version-node').hide();
+    } else if (selectedValue === 'node') {
+        console.log('node happened!');
+        $('.api-version-node').show();
+        $('.api-version-curl').hide();
+    }
+});
+
+$(document).on('change', '.request-types', function(eventData) {
+    var selectedValue = $('.request-types').val();
     console.log(selectedValue);
     if (selectedValue === 'get') {
         requestify.getReq("https://api.airtable.com/v0/app9uvKeuVL1pOfCD/Restaurants?limit=3&view=Main%20View?forceInsecureCrossDomain=ALLOW_ANY_DOMAIN", "keyrpG4FPpEqZ0ubg", function(json){
