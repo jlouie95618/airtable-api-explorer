220dceb133e34c50e9ca2a832b46432b4f9b26fa
John Louie <jlouie95618@gmail.com>
2015-07-24 17:04:47 -0700

    changed jQuery behavior

--- js/test.js
+++ js/test.js
@@ -5,31 +5,41 @@ console.log(_);
 
 var requestify = require('./index.js');
 
-$(document).on('change', '.api-version', function(eventData) {
-    var selectedValue = $('.api-version').val();
-    if (selectedValue === 'curl') {
-        console.log('curl happened!');
-        $('.api-version-curl').show();
-        $('.api-version-node').hide();
-    } else if (selectedValue === 'node') {
-        console.log('node happened!');
-        $('.api-version-node').show();
-        $('.api-version-curl').hide();
-    }
-});
+$(document).ready(function() {
+    
+    //Place for jQuery code!!!
 
-$(document).on('change', '.request-types', function(eventData) {
-    var selectedValue = $('.request-types').val();
-    console.log(selectedValue);
-    if (selectedValue === 'get') {
-        requestify.getReq("https://api.airtable.com/v0/app9uvKeuVL1pOfCD/Restaurants?limit=3&view=Main%20View?forceInsecureCrossDomain=ALLOW_ANY_DOMAIN", "keyrpG4FPpEqZ0ubg", function(json){
-            console.log(JSON.stringify(3, null, json));
-        });
-    } else if (selectedValue === 'post') {
+    $('.api-version').change(function(eventData) {
+        var selectedValue = $('.api-version').val();
+        if (selectedValue === 'curl') {
+            console.log('curl happened!');
+            $('.api-version-curl').show();
+            $('.api-version-node').hide();
+        } else if (selectedValue === 'node') {
+            console.log('node happened!');
+            $('.api-version-node').show();
+            $('.api-version-curl').hide();
+        }
+    });
 
-    } else if (selectedValue === 'patch') {
+    $('.request-types').change(function(eventData) {
+        var selectedValue = $('.request-types').val();
+        console.log(selectedValue);
+        if (selectedValue === 'get') {
+            requestify.getReq("https://api.airtable.com/v0/app9uvKeuVL1pOfCD/Restaurants?limit=3&view=Main%20View?forceInsecureCrossDomain=ALLOW_ANY_DOMAIN", "keyrpG4FPpEqZ0ubg", function(json){
+                console.log(JSON.stringify(3, null, json));
+            });
+        } else if (selectedValue === 'post') {
 
-    } else if (selectedValue === 'delete') {
+        } else if (selectedValue === 'patch') {
 
-    }
+        } else if (selectedValue === 'delete') {
+
+        }
+    });
+
+    $('.send-button').click(function(eventData) {
+        console.log('send button pressed!');
+    });
+
 });
